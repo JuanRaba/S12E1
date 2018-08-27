205a62548a4f56da32045acdfff59068ce577f9b
juanraba <jrabanillog@gmail.com>
2018-08-27 13:37:37 -0300

    added elegant...remember erb, styles conflict

--- proyectoPlantilla/config/initializers/assets.rb
+++ proyectoPlantilla/config/initializers/assets.rb
@@ -12,9 +12,11 @@ Rails.application.config.assets.paths << Rails.root.join('vendor', 'bootstrap', 
 Rails.application.config.assets.paths << Rails.root.join('vendor', 'img')
 Rails.application.config.assets.paths << Rails.root.join('vendor', 'font-awesome', 'css')
 Rails.application.config.assets.paths << Rails.root.join('vendor', 'font-awesome', 'fonts')
+Rails.application.config.assets.paths << Rails.root.join('vendor', 'elegant-font', 'code')
+Rails.application.config.assets.paths << Rails.root.join('vendor', 'elegant-font', 'code', 'fonts')
 
 # Precompile additional assets.
 # application.js, application.css, and all non-JS/CSS in the app/assets
 # folder are already added.
 # Rails.application.config.assets.precompile += %w( admin.js admin.css )
-Rails.application.config.assets.precompile += %w(*.css *.png *.gif *.jpg)
+Rails.application.config.assets.precompile += %w(*.css *.png *.gif *.jpg *.*)
